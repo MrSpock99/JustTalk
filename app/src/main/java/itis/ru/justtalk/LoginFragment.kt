8c5cb214bc77092c2c7ddf7a31e2acd31d2d9850
MrSpock99 <khusnut99@gmail.com>
2019-03-30 16:25:38 +0300

    add login fragment and xml(mock)

---
+++ app/src/main/java/itis/ru/justtalk/LoginFragment.kt
@@ -0,0 +1,30 @@
+package itis.ru.justtalk
+
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class LoginFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = LoginFragment()
+    }
+
+    private lateinit var viewModel: LoginViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/itis/ru/justtalk/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package itis.ru.justtalk
+
+import android.arch.lifecycle.ViewModel;
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
