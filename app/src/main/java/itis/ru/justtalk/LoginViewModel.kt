4262c26b9ec64806ebd2051ff83e1aa05aaeb4e0
MrSpock99 <khusnut99@gmail.com>
2019-03-31 17:19:54 +0300

    add shitty login

--- app/src/main/java/itis/ru/justtalk/LoginViewModel.kt
+++ app/src/main/java/itis/ru/justtalk/LoginViewModel.kt
@@ -1,7 +1,24 @@
 package itis.ru.justtalk
 
-import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModel
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
 
-class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class LoginViewModel(private val mGoogleApiClient: GoogleApiClient) : ViewModel(),
+    GoogleApiClient.OnConnectionFailedListener {
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun firebaseGoogleAuth() {
+        val signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
+        /* val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+             .requestEmail()
+             .build()
+         mGoogleApiClient = GoogleApiClient.Builder(this)
+             .enableAutoManage(this *//* FragmentActivity *//*, this *//* OnConnectionFailedListener *//*)
+            .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+            .build()*/
+    }
 }
--- app/src/main/java/itis/ru/justtalk/MainActivity.kt
+++ app/src/main/java/itis/ru/justtalk/MainActivity.kt
@@ -1,12 +1,13 @@
 package itis.ru.justtalk
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.main_container, LoginFragment.newInstance()).commit()
     }
 }
