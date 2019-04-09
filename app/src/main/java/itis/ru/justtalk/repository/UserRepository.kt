aa747ee72fe0cbb334466248b456e45af5254f46
MrSpock99 <khusnut99@gmail.com>
2019-04-09 15:26:45 +0300

    refactor: UserRepository

--- app/src/main/java/itis/ru/justtalk/repository/UserRepository.kt
+++ app/src/main/java/itis/ru/justtalk/repository/UserRepository.kt
@@ -1,10 +1,10 @@
 package itis.ru.justtalk.repository
 
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.AuthResult
 import io.reactivex.Maybe
 
 interface UserRepository {
-    fun login(account: GoogleSignInAccount): Maybe<FirebaseUser>
+    fun login(account: GoogleSignInAccount): Maybe<AuthResult>
     fun addUserToDb(age: Int, gender: String, location: HashMap<String, Double>)
 }
--- app/src/main/java/itis/ru/justtalk/repository/UserRepositoryImpl.kt
+++ app/src/main/java/itis/ru/justtalk/repository/UserRepositoryImpl.kt
@@ -2,17 +2,23 @@ package itis.ru.justtalk.repository
 
 import android.util.Log
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.GeoPoint
 import durdinapps.rxfirebase2.RxFirebaseAuth
 import io.reactivex.Maybe
 
+private const val USER_NAME = "name"
+private const val USER_AGE = "age"
+private const val USER_GENDER = "gender"
+private const val USER_LOCATION = "location"
+private const val USERS = "users"
+
 class UserRepositoryImpl(private val mFirebaseAuth: FirebaseAuth, private val db: FirebaseFirestore) : UserRepository {
 
-    override fun login(account: GoogleSignInAccount): Maybe<FirebaseUser> {
+    override fun login(account: GoogleSignInAccount): Maybe<AuthResult> {
         /*return Single.create { emitter: Compleatable ->
             mFirebaseAuth.signInWithCredential(
                 GoogleAuthProvider.getCredential(
@@ -30,20 +36,16 @@ class UserRepositoryImpl(private val mFirebaseAuth: FirebaseAuth, private val db
         */return RxFirebaseAuth.signInWithCredential(
                 mFirebaseAuth,
                 GoogleAuthProvider.getCredential(account.idToken, null)
-        ).map {
-            //addUserToDb(account)
-            it.user
-            //добавить в бд
-        }
+        )
     }
 
     override fun addUserToDb(age: Int, gender: String, location: HashMap<String, Double>) {
         val userMap = HashMap<String, Any>()
-        userMap["name"] = FirebaseAuth.getInstance().currentUser?.displayName.toString()
-        userMap["age"] = age
-        userMap["gender"] = gender
-        userMap["location"] = GeoPoint(location["lat"] ?: 0.0, location["lon"] ?: 0.0)
-        db.collection("users")
+        userMap[USER_NAME] = FirebaseAuth.getInstance().currentUser?.displayName.toString()
+        userMap[USER_AGE] = age
+        userMap[USER_GENDER] = gender
+        userMap[USER_LOCATION] = GeoPoint(location["lat"] ?: 0.0, location["lon"] ?: 0.0)
+        db.collection(USERS)
                 .add(userMap)
                 .addOnSuccessListener {
                     Log.d("MYLOG", it.toString())
