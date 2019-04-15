c3bb719c3c0f1180b7eae286bf0469a7fd7d9564
MrSpock99 <khusnut99@gmail.com>
2019-04-16 00:51:44 +0300

    add myprofile xml

---
+++ app/src/main/java/itis/ru/justtalk/ui/myprofile/MyProfileFragment.kt
@@ -0,0 +1,14 @@
+package itis.ru.justtalk.ui.myprofile
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import itis.ru.justtalk.R
+
+class MyProfileFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+    }
+}
---
+++ app/src/main/java/itis/ru/justtalk/ui/myprofile/MyProfileViewModel.kt
@@ -0,0 +1,7 @@
+package itis.ru.justtalk.ui.myprofile
+
+import android.arch.lifecycle.ViewModel
+
+class MyProfileViewModel : ViewModel() {
+
+}
