c2ce7a132d1ac48a2908271899d8555fbe7f29dd
MrSpock99 <khusnut99@gmail.com>
2019-05-05 08:29:35 +0300

    get distance from location

--- app/src/main/java/itis/ru/justtalk/utils/Utils.kt
+++ app/src/main/java/itis/ru/justtalk/utils/Utils.kt
@@ -1,7 +1,24 @@
 package itis.ru.justtalk.utils
 
+import android.location.Location
 import com.google.firebase.firestore.GeoPoint
+import kotlin.math.roundToInt
 
-fun getDistanceFromLocation(geoPoint: GeoPoint): String{
-    return "nearby"
+fun getDistanceFromLocation(geoPoint: GeoPoint, myLocation: GeoPoint): String {
+    val pointA = Location("").apply {
+        latitude = geoPoint.latitude
+        longitude = geoPoint.longitude
+    }
+    val pointB = Location("").apply {
+        latitude = myLocation.latitude
+        longitude = myLocation.longitude
+    }
+
+    val distanceInMeters = pointB.distanceTo(pointA)
+
+    return if (distanceInMeters < 1000) {
+        "$distanceInMeters m"
+    } else {
+        "${(distanceInMeters / 1000).roundToInt()} km"
+    }
 }
