9bfbcaec6072dc6b5cd600d4d6a8fe12c4729734
MrSpock99 <khusnut99@gmail.com>
2019-05-14 05:53:06 +0300

    add TestFragment xml

---
+++ app/src/main/java/itis/ru/justtalk/ui/words/test/TestFragment.kt
@@ -0,0 +1,7 @@
+package itis.ru.justtalk.ui.words.test
+
+import itis.ru.justtalk.ui.base.BaseFragment
+
+class TestFragment: BaseFragment() {
+
+}
---
+++ app/src/main/java/itis/ru/justtalk/ui/words/test/TestViewModel.kt
@@ -0,0 +1,6 @@
+package itis.ru.justtalk.ui.words.test
+
+import itis.ru.justtalk.ui.base.BaseViewModel
+
+class TestViewModel: BaseViewModel() {
+}
