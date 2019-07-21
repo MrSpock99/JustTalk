93eb9a664a6597c25e13cd4c747b0d928945b54c
MrSpock99 <khusnut99@gmail.com>
2019-07-21 11:27:41 +0300

    add test for group

--- app/src/main/java/itis/ru/justtalk/ui/words/words/WordsFragment.kt
+++ app/src/main/java/itis/ru/justtalk/ui/words/words/WordsFragment.kt
@@ -6,9 +6,7 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import itis.ru.justtalk.BaseApplication
 import itis.ru.justtalk.R
 import itis.ru.justtalk.adapters.WordsAdapter
@@ -18,6 +16,7 @@ import itis.ru.justtalk.ui.base.REQUEST_CODE
 import itis.ru.justtalk.ui.words.groups.ARG_GROUP_ID
 import itis.ru.justtalk.ui.words.groups.ARG_GROUP_NAME
 import itis.ru.justtalk.ui.words.groups.ARG_IMAGE_URL
+import itis.ru.justtalk.ui.words.test.TestFragment
 import itis.ru.justtalk.utils.ViewModelFactory
 import kotlinx.android.synthetic.main.fragment_words.*
 import javax.inject.Inject
@@ -64,6 +63,23 @@ class WordsFragment : BaseFragment() {
         viewModel.getWords(groupId)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.menu_add, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.menu_add -> {
+                startActivityForResult(
+                    Intent(rootActivity, AddWordActivity::class.java),
+                    REQ_CODE_ADD_WORD
+                )
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK) {
@@ -82,11 +98,10 @@ class WordsFragment : BaseFragment() {
         setToolbarAndBottomNavVisibility(View.VISIBLE, View.GONE)
         setArrowToolbarVisibility(true)
         setToolbarTitle(arguments?.get(ARG_GROUP_NAME) as String)
-        fab_add_word.setOnClickListener {
-            startActivityForResult(
-                Intent(rootActivity, AddWordActivity::class.java),
-                REQ_CODE_ADD_WORD
-            )
+        btn_start_test.setOnClickListener {
+            val bundle = Bundle()
+            bundle.putLong(ARG_GROUP_ID, groupId)
+            rootActivity.navigateTo(TestFragment.toString(), bundle)
         }
     }
 
